2918a0af36f79ab3368eb3d06443290ed3cee1ef
Tran Le Khanh Toan <trantoan.tratoa@gmail.com>
2026-06-26 19:50:44 +0700

    [Toan/26-6] feat: Add ViewedProductsHistory class

--- src/service/ViewedProductsHistory.java
+++ src/service/ViewedProductsHistory.java
@@ -8,6 +8,110 @@ package service;
  *
  * @author ADMIN
  */
+
+import java.util.HashMap;
+
 public class ViewedProductsHistory {
-    
+    private Node head;
+    private HashMap<String, Node> productMap;
+
+    public ViewedProductsHistory() {
+        this.head = null;
+        this.productMap = new HashMap<>();
+    }
+
+    /**
+     * Method to check if history is empty
+     */
+    public boolean isEmpty() {
+        return this.head == null;
+    }
+
+    /**
+     * Method to clear history
+     */
+    public void clear() {
+        this.head = null;
+        this.productMap.clear();
+    }
+
+    /**
+     * Method to add a viewed product to the most recent
+     */
+    public void addViewedProduct(Product product) {
+        String productID = product.getId();
+        Node node = this.productMap.get(productID);
+
+        if (node != null) { // Product has already been viewed before
+            if (node != head) { // Mitigate operations if the viewed product is already the most recent
+                // Temporarily delete the Node from the Linked List
+                this.removeNode(node);
+
+                node.prev = null;
+                node.next = head;
+            }
+        } else { // Product has not been viewed until now
+            // Instantiate new Node for the Linked List and insert a new entry into the Hash Map
+            node = new Node(product, null, this.head);
+            this.productMap.put(productID, node);
+        }
+
+        if (node != head) {
+            // Set the Node as the head of the Linked List
+            if (!this.isEmpty())
+                this.head.prev = node;
+
+            this.head = node;
+        }
+    }
+
+    /**
+     * Method to remove a viewed product
+     */
+    public void removeViewedProduct(Product product) {
+        String productID = product.getId();
+        this.removeViewedProduct(productID);
+    }
+
+    /**
+     * Method to remove a viewed product by ID
+     */
+    public void removeViewedProduct(String productID) {
+        Node node = this.productMap.get(productID);
+
+        if (node != null) {
+            this.removeNode(node);
+            this.productMap.remove(productID);
+        }
+    }
+
+    /**
+     * Private method to remove a node in the Linked List
+     */
+    private void removeNode(Node node) {
+        if (node.prev != null) { // The Node is not the most recent
+            node.prev.next = node.next;
+        } else { // The Node is the most recent
+            this.head = node.next;
+        }
+
+        if (node.next != null) { // The Node is not the least recent
+            node.next.prev = node.prev;
+        }
+    }
+
+    /**
+     * Internal Node used in the Linked List
+     */
+    private static class Node {
+        Product product;
+        Node prev;
+        Node next;
+
+        Node(Product product, Node prev, Node next) {
+            this.product = product;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
 }
